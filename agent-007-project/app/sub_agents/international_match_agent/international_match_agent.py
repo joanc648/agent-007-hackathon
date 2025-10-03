b99d4bcb903245acf38d36a91aeff98f30a01ba3
Kavin Velmurugan <34014055+Kavelin@users.noreply.github.com>
2025-10-03 15:36:14 -0400

    Update international_match_agent.py

--- agent-007-project/app/sub_agents/international_match_agent/international_match_agent.py
+++ agent-007-project/app/sub_agents/international_match_agent/international_match_agent.py
@@ -25,42 +25,22 @@ os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
 os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 
-def get_weather(query: str) -> str:
-    """Simulates a web search. Use it get information on weather.
-
-    Args:
-        query: A string containing the location to get weather information for.
-
-    Returns:
-        A string with the simulated weather information for the queried location.
+def get_international_match(query: str) -> str:
     """
-    if "sf" in query.lower() or "san francisco" in query.lower():
-        return "It's 60 degrees and foggy."
-    return "It's 90 degrees and sunny."
-
-
-def get_current_time(query: str) -> str:
-    """Simulates getting the current time for a city.
-
-    Args:
-        city: The name of the city to get the current time for.
-
-    Returns:
-        A string with the current time information.
+    TODO: Finds an international match for the donor.
     """
-    if "sf" in query.lower() or "san francisco" in query.lower():
-        tz_identifier = "America/Los_Angeles"
-    else:
-        return f"Sorry, I don't have timezone information for query: {query}."
+    
+    return "Could not find an international match."
 
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    return f"The current time for query {query} is {now.strftime('%Y-%m-%d %H:%M:%S %Z%z')}"
 
-# 
+PROMPT = 
+"""
+You are an international area organ matcher. Go through global databases and find a match for the donor/donee.
+"""
+
 root_agent = Agent(
     name="international_match_agent",
     model="gemini-2.5-flash", # Flexible model choice i.e. chatgpt, claude, deepseek etc.
-    instruction="You are a helpful AI assistant designed to provide accurate and useful information.",
+    instruction=PROMPT,
     tools=[get_weather, get_current_time],
 )
